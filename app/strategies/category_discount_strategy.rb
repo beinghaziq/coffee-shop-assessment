2da75a05c1c0ac766aa5ff510c37da98846d15c6
Muhammad Haziq <beinghaziq@gmail.com>
2024-09-18 22:11:56 +0500

    order update fixed

--- app/strategies/category_discount_strategy.rb
+++ app/strategies/category_discount_strategy.rb
@@ -1,9 +1,11 @@
 class CategoryDiscountStrategy < DiscountStrategy
   def apply(order, discount)
     amount = if discount.type == "percentage"
-      (order.items.where(category_id: discount.discountable_id).reduce(0) { |sum, item| sum + ((item.amount * item.quantity(order.id)) * (discount.amount / 100)) })
+      (order.items.where(category_id: discount.discountable_id).reduce(0)
+        { |sum, item| sum + ((item.amount * item.quantity(order.id)) * (discount.amount / 100)) })
     else
-      (order.items.where(category_id: discount.discountable_id).reduce(0) { |sum, item| sum + (discount.amount * item.quantity(order.id)) })
+      (order.items.where(category_id: discount.discountable_id).reduce(0)
+      { |sum, item| sum + (discount.amount * item.quantity(order.id)) })
     end
 
     order.update(discounted_amount: order.discounted_amount - amount)
--- app/strategies/combo_discount_strategy.rb
+++ app/strategies/combo_discount_strategy.rb
@@ -2,9 +2,11 @@ class ComboDiscountStrategy < DiscountStrategy
   def apply(order, discount)
    amount = if combo_in_order?(order, discount.discountable)
       if discount.type == "percentage"
-        (discount.discountable.items.where(id: discount.discountable_id).reduce(0) { |sum, item| sum + ((item.amount * item.quantity(order.id)) * (discount.amount / 100)) })
+        (discount.discountable.items.where(id: discount.discountable_id).reduce(0)
+          { |sum, item| sum + ((item.amount * item.quantity(order.id)) * (discount.amount / 100)) })
       else
-        (discount.discountable.items.where(id: discount.discountable_id).reduce(0) { |sum, item| sum + (discount.amount * item.quantity(order.id)) })
+        (discount.discountable.items.where(id: discount.discountable_id).reduce(0)
+          { |sum, item| sum + (discount.amount * item.quantity(order.id)) })
       end
     end
 
